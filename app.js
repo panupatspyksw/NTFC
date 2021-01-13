e0d8797284993c8249c099a93dc4872ddebd7ef1
panupat <67039010+panupatspyksw@users.noreply.github.com>
2021-01-13 16:00:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -16,12 +16,38 @@ const nodemailer = require('nodemailer');
 var defaultimg = "defaultprofile.png";
 
 
-var connection = mysql.createConnection({
+var connection
+var db_config = {
 	host     : 'us-cdbr-east-02.cleardb.com',
 	user     : 'bbc5aa79adb978',
 	password : 'bc3f80a0',
 	database : 'heroku_967c364b1d024e2'
-});
+  };
+  
+  
+  function handleDisconnect() {
+	connection = mysql.createConnection(db_config); // Recreate the connection, since
+													// the old one cannot be reused.
+  
+	connection.connect(function(err) {              // The server is either down
+	  if(err) {                                     // or restarting (takes a while sometimes).
+		console.log('error when connecting to db:', err);
+		setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+	  }                                     // to avoid a hot loop, and to allow our node script to
+	});                                     // process asynchronous requests in the meantime.
+											// If you're also serving http, display a 503 error.
+	connection.on('error', function(err) {
+	  console.log('db error', err);
+	  if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+		handleDisconnect();                         // lost due to either server restart, or a
+	  } else {                                      // connnection idle timeout (the wait_timeout
+		throw err;                                  // server variable configures this)
+	  }
+	});
+  }
+  
+  handleDisconnect();
+
 // var connection = mysql.createConnection({
 // 	host     : 'localhost',
 // 	user     : 'root',
